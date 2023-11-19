5567b909de50f18753a30eff9aa63426085a8cef
vikahenkel <vhenkel39@gmail.com>
2023-11-19 18:12:24 +0100

    Домашка сдана, но было пару недороботок...

--- src/main/java/pro/sky/HAufgabe24/CalculatorController.java
+++ src/main/java/pro/sky/HAufgabe24/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/calculator")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
@@ -14,25 +15,43 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @RequestMapping (path = "/calculator")
+    @GetMapping
     public String hello(){
         return calculatorService.hello();
     }
     /* /calculator/plus?num1=5&num2=5*/
-    @GetMapping(path = "/calculator/plus")
-    public String plus(@RequestParam("num1") int a, @RequestParam ("num2")int b) {
-        return a + " + " + b + " = " + calculatorService.plus (a,b);
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Integer a,
+                       @RequestParam(value = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Ошибка! не передан параметр";
+        }
+            return a + " + " + b + " = " + calculatorService.plus (a,b);
     }
-    @GetMapping(path = "/calculator/minus")
-    public String minus(@RequestParam ("num1") int a, @RequestParam ("num2")int b) {
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Integer a,
+                        @RequestParam(value = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Ошибка! не передан параметр";
+        }
         return a + " - " + b + " = " + calculatorService.minus (a,b);
     }
-    @GetMapping(path = "/calculator/multiply")
-    public String multiply(@RequestParam ("num1") int a, @RequestParam ("num2")int b) {
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Integer a,
+                           @RequestParam(value = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Ошибка! не передан параметр";
+        }
         return a + " * " + b + " = " + calculatorService.multiply (a,b);
     }
-    @GetMapping(path = "/calculator/divide")
-    public String divide(@RequestParam ("num1") int a, @RequestParam ("num2")int b) {
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Integer a,
+                         @RequestParam(value = "num2", required = false) Integer b) {
+        if (a == null || b == null) {
+            return "Ошибка! не передан параметр";
+        }
+        if (b == 0){
+            return "Делить на ноль нельзя!";        }
         return a + " / " + b + " = " + calculatorService.divide (a,b);
     }
 }
